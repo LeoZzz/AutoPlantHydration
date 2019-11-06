c89349719ea8c41dd6b9dbba685f8bc105a80bdf
ALazenka <andrewlazenka@gmail.com>
2019-11-05 19:42:55 -0500

    Big update to minicom script after testing with basys board

--- minicom.py
+++ minicom.py
@@ -1,164 +1,115 @@
-import time
-import sched
-import grovepi
-import subprocess
-import math
-import RPi.GPIO as GPIO
 import requests
+import RPi.GPIO as GPIO
+import serial
+import time
 
-# Config
+# GPIO outpins
+pump = 14
+leds = 15
+piezo = 16
+
+# GPIO config
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(pump, GPIO.OUT)
+GPIO.setup(leds, GPIO.OUT)
+GPIO.setup(piezo, GPIO.OUT)
+
+# Script config values
 config_moisture_threshold = 250
+config_light_threshold = 250
 config_water_time = 1
 
-# Schedules
-config_pump_time = 60
-config_upload_time = 1800
+# Serial data config
+ser = serial.Serial(
+    port='/dev/ttyUSB1',
+    baudrate = 9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+)
 
-# Pushover config
-token = "pushoverToken"
+# Reads in a chunk of data from the serial port
+# Terminates a line when an empty array of data is sent
+def read_sensor_data():
+    # remains true until empty array is sent
+    continue_reading = True
+    # Data chunk
+    line = ""
+    while continue_reading:
+        # Read a single line from the serial port
+        new_line = ser.readline()
+        # check if current line is empty array
+        if not new_line.strip():
+            # break loop
+            continue_reading = False
+        else:
+            # append current line to chunk of data
+            line = line + new_line
 
-users = {
-    "userToken": "deviceName",
-}
+    split_data = line.split()
+    # some chunks sent do not include valid sensor data
+    # default response is to send a negative value
+    if len(split_data) < 21:
+        return [-1, -1]
 
-# Pins for GPIO
-pump_1 = 14
-pump_2 = 15
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(pump_1, GPIO.OUT)
-GPIO.setup(pump_2, GPIO.OUT)
-GPIO.output(pump_1,1)
-GPIO.output(pump_2,1)
+    light_sensor = split_data[17]
+    moisture_sensor = split_data[20]
+    return [light_sensor, moisture_sensor]
 
-# Pins for GROVE atmega
-light_sensor = 0
-mositure_sensor_1 = 1
-mositure_sensor_2 = 2
-temp_humidity_sensor = 6
+# activates pump for a given amount of time when moisture
+# sensor reads under a certain threshold
+def test_moisture(m):
+    if m < config_moisture_threshold:
+        # GPIO.output(pump, 0)
+        # time.sleep(config_water_time)
+        # GPIO.output(pump, 1)
+        print "Plant has been watered!"
 
-# Scheduler
-scheduler = sched.scheduler(time.time, time.sleep)
+# Posts sensor data to web service
+def upload_data(light, moisture):
+    r = requests.post(
+        "http://localhost:3000/api/sensor-data",
+        data = {
+            'light': light,
+            'moisture': moisture,
+        }
+    );
 
-# Send post data
-def send_pushover_message(title, message, sound):
-    for key in users:
-        r = requests.post(
-            "https://api.pushover.net/1/messages.json",
-            data = {
-                'token': token,
-                'html': 1,
-                'title': title,
-                'sound': sound,
-                'message': message,
-                'user': key,
-                'device': users[key]
-            }
-        );
+    if r.status_code == 200:
+        print "Data uploaded to web service"
 
-        print(r.status_code, r.reason)
+# Prints a friendly text version of the status of the
+# moisture sensor
+# used for console print and push notification
+def moisture_level(m):
+    if m > config_moisture_threshold * 2:
+        return "hydrated"
+    elif m <= config_moisture_threshold * 2 and m > config_moisture_threshold:
+        return "needs water soon"
+    elif m < config_moisture_threshold:
+        return "needs water now"
 
-# Moisture reading to
-def moisture_level_str(level):
-    if level > 600:
-        return "freshly watered"
-    elif level < config_moisture_threshold:
-        return "thirsty"
-    else:
-        return "happy"
-
-# Moisture reading to
-def light_level_str(level):
-    if level > 500:
+# Prints a friendly text version of the status of the
+# moisture sensor
+# used for console print and push notification
+def light_level(l):
+    if l > config_light_threshold * 2:
         return "bright"
-    elif level < 150:
+    elif l <= config_light_threshold * 2 and l > config_light_threshold:
         return "dark"
-    else:
+    elif l < config_light_threshold:
         return "dim"
 
-# Read the data from the sensors
-def read_sensor():
+# Main script loop
+while True:
+    [light, moisture] = read_sensor_data()
+    print "Moisture status of plant: " + moisture_level(moisture)
+    print "Light level on plant: " + light_level(light)
+    test_moisture(moisture)
+
     try:
-        # Read moisture
-        moisture1 = grovepi.analogRead(mositure_sensor_1)
-        moisture2 = grovepi.analogRead(mositure_sensor_2)
-
-        # Read light
-        light = grovepi.analogRead(light_sensor)
-
-        # Read temp and humidity
-        [temp, humidity] = grovepi.dht(temp_humidity_sensor, 0)
-
-        # Return -1 in case of bad temp/humidity sensor reading
-        # temp/humidity sensor sometimes gives nan
-        if math.isnan(temp) or math.isnan(humidity):
-            return [-1, -1, -1, -1, -1]
-
-        return [moisture1, moisture2, light, temp, humidity]
-
-    # Return -1 in case of sensor error
-    except IOError as TypeError:
-        return [-1, -1, -1, -1, -1]
-
-# Activate pump
-def activate_pumps(sc):
-    [moisture_1, moisture_2, light,temp, humidity] = read_sensor()
-
-    # Message; POST to pushover
-    message = "";
-
-    # Check if they need water
-    # Pump 1
-    if moisture_1 < config_moisture_threshold:
-        GPIO.output(pump_1, 0)
-        time.sleep(config_water_time)
-        GPIO.output(pump_1, 1)
-        message += "<b>Plant 1</b> has been watered.<br>"
-
-    # Pump 2
-    if moisture_2 < config_moisture_threshold:
-        GPIO.output(pump_2, 0)
-        time.sleep(config_water_time)
-        GPIO.output(pump_2, 1)
-        message += "<b>Plant 2</b> has been watered.<br>"
-
-    # Send POST
-    if message != "":
-        send_pushover_message("Plant Update", message, "pushover")
-
-    # Retrigger function
-    sc.enter(config_pump_time, 1, activate_pumps, (sc,))
-
-# Activate pump
-def upload_readings(sc):
-    # Print
-    [moisture_1, moisture_2, light, temp, humidity] = read_sensor()
-
-    if moisture_1 == -1:
-        print("Sensor Error!")
-    else:
-        curr_time = time.strftime("%Y-%m-%d:%H-%M-%S")
-        print(("Time:%s\nMoisture: %d\nMoisture2: %d\nLight: %d\nTemp: %.2f\nHumidity:%.2f\n" %(curr_time,moisture_1,moisture_2,light,temp,humidity)))
-
-        message = \
-            "The temperature is " + str(temp) + "°C.<br>" + \
-            "The relative humidity is " + str(humidity) + "%.<br>" + \
-            "The light level is " + light_level_str(light) + ".<br>" + \
-            "Plant 1 is " + moisture_level_str(moisture_1) + ".<br>" + \
-            "Plant 2 is " + moisture_level_str(moisture_2) + ".<br>"
-
-        send_pushover_message("APWS Update", message, "none")
-
-    # Retrigger function
-    sc.enter(config_upload_time, 1, upload_readings, (sc,))
-
-# Main
-try:
-    # Run scheduled tasks
-    scheduler.enter(config_pump_time, 1, activate_pumps, (s,))
-    scheduler.enter(config_upload_time, 1, upload_readings, (s,))
-    scheduler.run()
-except Exception as ex:
-    print ("Exception occured:" + ex.__name__)
-finally:
-    # Deactivate pins
-    GPIO.cleanup()
+        upload_data(light, moisture)
+    except Exception as ex:
+        print "There was an error uploading the sensor data"
